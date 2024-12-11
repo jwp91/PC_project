1850bb14f6773752e5dd420849fe1d633cef458d
jwp91 <jaredwp91@gmail.com>
2024-12-11 08:14:19 -0700

    Edits from Tuesday night, tuning the PID loop.

--- Software/CascadeController.py
+++ Software/CascadeController.py
@@ -6,6 +6,7 @@ from hcsr04 import HCSR04
 from MPU6050 import MPU6050
 from mg90s import servo
 import pc
+pi = 3.141592
 
 #----- Pinout declaration
 # Joystick pins
@@ -33,7 +34,7 @@ xButton.atten(xButton.ATTN_11DB)
 xButtZero = xButton.read()
 yButton = ADC(Pin(pinVry, Pin.IN)) # create an ADC object acting on a pin
 yButton.atten(yButton.ATTN_11DB)
-yButtZero = yButton.read()
+yButtZero = 7436
 button = Pin(pinSW, Pin.IN, Pin.PULL_UP)
 
 # Ultrasonics
@@ -52,14 +53,14 @@ maxAngle = 2.65
 def boundsOK():
     angle = mpu.read_angle()['y']
     if angle<minAngle:
-        motor.set_speed(0.08)
+        motor.set_speed(-0.1)
         return False
     if angle>maxAngle:
-        motor.set_speed(-0.08)
+        motor.set_speed(0.1)
         return False
     return True
 
-zero = 2.27 # Must be experimentally determined
+zero = 2.29 # Must be experimentally determined
 tolerance = 0.04 # radians
 def center():
     ready = input("Begin leveling sequence?")
@@ -71,13 +72,14 @@ def center():
     while abs(diff)>tolerance: 
         try:
             angle = mpu.read_angle()['y']
-            diff = zero-angle
+            diff = angle-zero
             print(f"Angle: {angle:.4g}; diff: {diff:.4g}")
             if boundsOK():
                 sign = diff/abs(diff)
-                motor.set_speed(sign*0.05)
+                motor.set_speed(sign*0.1)
                 time.sleep(0.02)
             else:
+                motor.set_speed(0)
                 raise ValueError("Bounds exceeded during centering.")
                 return None
         except KeyboardInterrupt:
@@ -87,31 +89,47 @@ def center():
     print("Initial leveling complete")
     return None
 
-L = 410 # mm
-def position():
-    s1 = sensor1.distance_mm()
-    time.sleep(0.0012) # Time for a ping at max distance
-    s2 = sensor2.distance_mm()
+L = 405 # mm
+posLast = L/2
+rad = 20 #mm
+def position(alpha = 0.1, output = False):
+    global posLast
+    s1 = sensor1.distance_mm() + rad
+    time.sleep(0.005) # Time for a ping to return at max distance
+    s2 = sensor2.distance_mm() + rad
+    
+    # Gate filter
     if s1 < 0:
         s1 = 0
     if s2 < 0:
         s2 = 0
+    if s1 > L:
+        s1 = L
+    if s2 > L:
+        s2 = L
     if s1<s2:
-        return s1
+        reading = s1
     else:
-        return L - s2
+        reading = L - s2
+    if abs(reading-posLast) > 150:
+        reading = posLast
+    posLast = alpha*reading + (1-alpha)*posLast
+    if output:
+        print(f"s1: {s1:.4g}, s2: {L-s2:.4g}, SP: {L/2}")
+    return posLast
     
 #----- Main loop
 center()
 mode = 'manual'
 # Outer PID configuration (position error -> angle setpoint)
-Kc_out = 150
+Kc_out = pi/250
 KI_out = 0
-KD_out = 450
+KD_out = 2*Kc_out
+# Optimization showed that KD_out ~= 2*Kc_out
 pidOuter = pc.PID(Kc_out, KI_out, KD_out, dt=0.1, \
              outputLimits=[minAngle, maxAngle])
 # Inner PID configuration (angle error -> motor speed)
-Kc_in = 10 # Simulation used a range of -25 to 25 instead of -1 to 1
+Kc_in = -6.4 # Simulation used a range of -25 to 25 instead of -1 to 1
 KI_in = 0.0
 KD_in = 0.0
 pidInner = pc.PID(Kc_in, KI_in, KD_in, dt = 0.1, \
@@ -119,48 +137,48 @@ pidInner = pc.PID(Kc_in, KI_in, KD_in, dt = 0.1, \
 startTime = time.time() # seconds
 reading = 0
 print("Mode: manual")
-while True:
-    try:
+try:
+    while True:
         if mode == 'manual':
-            yMaxRead = 700
-            stick = (yButtZero-yButton.read())/yMaxRead
-            if abs(stick) < 0.2:
+            stick = yButton.read()-yButtZero
+            if abs(stick) < 100:
                 stick = 0
             reading = max(min(1, stick), -1)
             if boundsOK():
                 motor.set_speed(reading)
             else:
                 print("Bounds error! Correcting...")
-            pos = position()
+            pos = position(output = False)
             thetaActual = mpu.read_angle()['y']
             newSpeed = reading
-            print(f"pos: {pos:.4g}, theta: {thetaActual:.4g}, speedSP: {newSpeed:.4g}")
+            print(f"pos: {pos:.4g}, posSP: {L/2:.4g}")
             if button.value() == 0 and (time.time()-startTime)%0.5<0.01:
                 # The extra bit in the "and" statement reduces the
                 # button's sensitivity by effectively only having it
                 # read every half second.
                 mode = 'auto'
                 print("Mode: auto")
-        elif mode == 'auto':            
-            SP = L/2000
-            pos = position() / 1000
+        elif mode == 'auto':
+            # Lengths should be meters to match PID params
+            SP = L/2
+            pos = position()
             pidOuter.setpoint = SP
-            thetaSP = pidOuter(pos)[0]
+            thetaSP, Pout, Iout, Dout = pidOuter(pos)
             pidInner.setpoint = thetaSP
             thetaActual = mpu.read_angle()['y']
             newSpeed = pidInner(thetaActual)[0]
             if boundsOK():
                 motor.set_speed(newSpeed)
             else:
-                print("Bounds error! Correcting...")
-            print(f"pos: {pos:.4g}, theta: {thetaActual:.4g}, speedSP: {newSpeed:.4g}")
+                pass
+                #print("Bounds error! Correcting...")
+            #print(f"pos: {pos:.4g}, posSP: {SP:.4g}")
+            print(f"thetaSP: {thetaSP:.4g}, theta: {thetaActual:.4g}")
             if button.value() == 0 and (time.time()-startTime)%0.5<0.01:
                 mode = 'manual'
                 print("Mode: manual")
         else:
             print("Unknown error...")
-    except KeyboardInterrupt:
-        print("Loop ended.")
-        motor.set_speed(0)
-        time.sleep(10)
-        break
+except KeyboardInterrupt:
+    print("Loop ended.")
+    motor.set_speed(0)
